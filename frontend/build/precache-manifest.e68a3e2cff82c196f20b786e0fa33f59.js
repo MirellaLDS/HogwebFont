84daf7604a2cb9924f77bca2a0726fd6b75bed84
Mirella Lins <mirella.lds@gmail.com>
2020-05-28 16:01:21 -0300

    add shell

---
+++ frontend/build/precache-manifest.e68a3e2cff82c196f20b786e0fa33f59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c452128dd935a6bdac1611230d5746da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0ad3b73d79c87a6d56",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "277a2b7af1e92c57bd7c",
+    "url": "/static/js/2.42286669.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42286669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0ad3b73d79c87a6d56",
+    "url": "/static/js/main.1e67c85f.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "f931a6557688be4e26e0805fd0d2af3a",
+    "url": "/static/media/dev.f931a655.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ test_command_shell/server.js
@@ -0,0 +1,30 @@
+//Imports
+const express = require('express')
+const Shell = require('node-powershell');
+
+//Initializer a shell instance
+const ps = new Shell({
+   executionPolicy: 'Bypass',
+   noProfile: true 
+});
+
+//Setup and configure app
+const app = express()
+const port = 5556
+
+//route request
+app.use('/', (req, res) => {
+    res.json("Hello from Node")
+
+    // ps.addCommand(`docker ps -a`);
+    // ps.invoke()
+    // .then(response => {
+    //     res.send(response)
+    // })
+    // .catch(err => {
+    //     res.json(err)
+    // });
+});
+
+//Initialize app
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
